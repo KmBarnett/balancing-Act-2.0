4d2e8ba65f8c6b9bf84c2cca7d8c8f66c223e417
kmbarnett <kmbarnett94@gmail.com>
2020-07-06 11:12:41 -0600

    Add table to dom

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 
 // packages
 import $ from 'jquery';
-import { everyPage } from './Veiws/AllPages.js'
+import { everyPage, dashboard } from './Veiws/AllPages.js'
 
 // images & styles
 import './css/base.scss';
@@ -22,13 +22,110 @@ import './images/icons/transactions.svg';
 
 const root = $('#root')
 const rootMain = $('#root-main')
+const userName = 'Bill';
+let bannerClosed = false
+let income = 2000;
+let expenses = 1500;
+let creditScore = 750;
+let accounts = [
+  {
+    type: 'savings',
+    company: 'USAA'
+  },
+  {
+    type: 'credit',
+    company: 'chase'
+  },
+  {
+    type: 'checking',
+    company: 'USAA'
+  },
+  {
+    type: 'credit',
+    company: 'USAA'
+  },
+];
+const transactions = []
 
-const renderPage = () => {
-  root.prepend( everyPage.nav)
+// render page
+const renderPage = page => {
+  rootMain.empty()
   rootMain.append(everyPage.header)
+  page === 'dashboard' && renderDashboard()
+}
+
+// render dashboard
+
+const renderAccountOverveiw = (node) => {
+  node.append(dashboard.accountOverveiw(income, expenses, creditScore))
+  $('#close-icon-js').on('click', function() {
+    bannerClosed = true
+    renderPage('dashboard')
+  })
+}
+
+const renderAccounts = (node) => {
+  node.append(dashboard.accounts)
+  accounts.forEach(account => {
+    $('#accounts-js').append(dashboard.account(account))
+  });
+}
+
+const totalsHelper = () => {
+  return transactions.reduce((totals, transaction) => {
+    console.log(totals);
+    if (transaction.inflow) {
+      totals.totalIn += transaction.inflow
+    }
+    if (transaction.outflow) {
+      totals.totalOut += transaction.outflow
+    }
+    return totals
+  }, {
+    totalIn: 0,
+    totalOut: 0,
+  })
+}
+
+const renderTransactions = (node) => {
+  node.append(dashboard.transactions(totalsHelper()))
+  $('#new-transaction-js').on('click', () => {
+    root.append(dashboard.newTransactionModle)
+    $('#log-transaction').on('click', (e) => {
+      e.preventDefault()
+      let date = $('#date-js').val()
+      let payee = $('#payee-js').val()
+      let inflow = $('#inflow-js').val()
+      let outflow = $('#outflow-js').val()
+      transactions.push({
+        date,
+        payee,
+        inflow: parseInt(inflow),
+        outflow: parseInt(outflow)
+      })
+      $('#new-transaction-modle').remove()
+      renderPage('dashboard')
+    })
+  })
+  transactions.forEach(transaction => {
+    const {date, payee, inflow, outflow} = transaction
+    $(dashboard.transaction(date, payee, inflow, outflow)).insertAfter('#table-heads')
+  });
+
+}
+
+const renderDashboard = () => {
+  rootMain.append(dashboard.columns)
+  const columnOne = $('#column-1')
+  const columnTwo = $('#column-2')
+  !bannerClosed && columnOne.append(dashboard.banner(userName))
+  renderAccountOverveiw(columnOne)
+  renderAccounts(columnOne)
+  renderTransactions(columnTwo)
 }
 
 
 $( document ).ready(function() {
-  renderPage()
+  root.prepend( everyPage.nav)
+  renderPage('dashboard')
 });
